915974026fa3d9bddc33cbf817522042461f9fee
emmac124 <emma.cooperhawkins@gmail.com>
2021-02-18 16:15:12 -0800

    completed styling

--- public-apis/src/index.js
+++ public-apis/src/index.js
@@ -6,12 +6,16 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { reducer } from './reducers/index';
-
+import { ThemeProvider } from 'styled-components';
+import theme from './theme/theme';
+ 
 const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
-  <Provider store={store}>    
-    <App />
-  </Provider>,
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>    
+      <App />
+    </Provider>
+  </ThemeProvider>,
   document.getElementById('root')
 );
